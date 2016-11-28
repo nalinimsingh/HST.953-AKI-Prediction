633508faed9254bba9d4fa77392bc70d3a1ff615
Nalini Singh <nmsingh@mit.edu>
2016-11-28 16:12:09 -0500

    Limit MAP to acceptable values

--- data_extraction/map.sql
+++ data_extraction/map.sql
@@ -1,10 +1,11 @@
 ﻿-- Create a table with MAP measurements for each patient
 
 DROP MATERIALIZED VIEW IF EXISTS map cascade;
-CREATE MATERIALIZED VIEW map AS(
+CREATE MATERIALIZED VIEW map AS
 
 SELECT DISTINCT subject_id, icustay_id, charttime, itemid, valuenum
 FROM chartevents
-WHERE valuenum IS NOT NULL AND itemid IN (220181, 220052, 225312, 224322, 6702, 443, 52, 456) 
-ORDER BY icustay_id, charttime, itemid
-);
+WHERE valuenum IS NOT NULL AND itemid IN (220181, 220052, 225312, 224322, 6702, 443, 52, 456)
+  AND 30 < valuenum AND valuenum < 200
+ORDER BY valuenum, icustay_id, charttime, itemid
+
